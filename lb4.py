962e00f6d462d7b439b893bfa5216936806bf1e6
goncharovDV <dimarick1@mail.ru>
2023-11-13 11:41:32 +0300

    lab5

---
+++ lb4.py
@@ -0,0 +1,8 @@
+c = input("vvedite svoe Imia ")
+
+print("Hello,", c)
+
+
+chislo = int(input("vvedite chislo "))
+chislo2 = chislo + 1
+print("Sleduechie chislo posle", chislo, "estb", chislo2 + 1)
